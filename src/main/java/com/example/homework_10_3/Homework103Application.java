f6fa6a62743860ebf42d8f4e6b9d4b95581878b6
Vera Lisitsa <nvk1984@mail.ru>
2023-11-10 19:40:49 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_10_3/Homework103Application.java
@@ -0,0 +1,14 @@
+package com.example.homework_10_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework103Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework103Application.class, args);
+
+    }
+}
---
+++ src/main/java/com/example/homework_10_3/repository/MyRepository.java
@@ -0,0 +1,58 @@
+package com.example.homework_10_3.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+public class MyRepository {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private String scriptFileName = "myScript.sql";
+
+    public MyRepository() {
+
+    }
+
+    public static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<String> getProductName(String name) {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", name);
+
+        List<String> products = namedParameterJdbcTemplate.query(read(scriptFileName), parameters,
+                (resultSet, rowNum) -> {
+                    String product_name = resultSet.getString(1);
+                    return product_name;
+                });
+        return products;
+    }
+}
