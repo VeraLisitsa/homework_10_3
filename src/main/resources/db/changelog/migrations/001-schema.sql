d417ec5d0fce7cb1a77f1a5fe749d89d0ca28544
Vera Lisitsa <nvk1984@mail.ru>
2023-11-10 21:50:24 +0300

    Using migrations

---
+++ src/main/resources/db/changelog/migrations/001-schema.sql
@@ -0,0 +1,16 @@
+create table customers(
+                          id serial primary key,
+                          name varchar(20) not null,
+                          surname varchar(30) not null,
+                          age int,
+                          phone varchar(10)
+);
+
+create table orders(
+                       id serial primary key,
+                       date date,
+                       customer_id int,
+                       product_name varchar(40),
+                       amount int,
+                       foreign key (customer_id) references customers(id)
+);
